277466685725f22e9b3662262954fd1b6d2ede3c
TayyabKhalid25 <tayyabkhalid2004@gmail.com>
2023-12-07 09:03:34 +0500

    Adding qt base and multimedia

---
+++ Music Player/Wahaj.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <iostream>
+// Seperate file for Wahaj so that no clashes while we push and pull from git.
+// We will merge the files at the end.
+// Write all functions here which will be called in main.cpp (Wahaj)
+
+// Function for loading gui
+
+// Function for play button
+
+// Function for pause button
+
+// Functions for forward and backward
+
+// Functions for playback modes
+
+// Function for browse files, conversion of mp3 files to wav or find ways to run mp3
+
+// Function for storing data (Playlists, songs on machine, recents) in files, and reading from files upon start of program (DBMS)
