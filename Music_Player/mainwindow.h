78ae343fc2b3a095b4b3f999ce2700f1138279a2
TayyabKhalid25 <tayyabkhalid2004@gmail.com>
2023-12-10 23:27:50 +0500

    First Patch

---
+++ Music_Player/mainwindow.h
@@ -0,0 +1,50 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QMediaPlayer>
+#include <QtWidgets>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class MainWindow;
+}
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void durationChanged(quint64 duration);
+
+    void positionChanged(quint64 progress);
+
+    void on_pushButton_PlayPause_clicked();
+
+    void on_pushButton_SeekForward_clicked();
+
+    void on_pushButton_SeekBackward_clicked();
+
+    void on_pushButton_Mute_clicked();
+
+    void on_actionOpen_triggered();
+
+    void on_Seek_sliderMoved(int value);
+
+    void on_Volume_valueChanged(int value);
+
+private:
+    void updateduration(quint64 duration);
+    Ui::MainWindow *ui;
+    QMediaPlayer *MPlayer;
+    QString FileName;
+    QAudioOutput *audioOutput;
+    quint64 Mduration;
+
+};
+#endif // MAINWINDOW_H
